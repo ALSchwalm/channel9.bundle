1ce4956d13f83f952c131abdc2deb14181b7d3c7
Adam Schwalm <adamschwalm@gmail.com>
2014-02-02 18:36:34 -0600

    Initial commit

---
+++ Contents/Code/__init__.py
@@ -0,0 +1,8 @@
+
+URL = 'http://channel9.msdn.com/'
+
+def Start():
+
+    ObjectContainer.title1 = 'Channel 9'
+    HTTP.CacheTime = CACHE_1HOUR
+    HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:23.0) Gecko/20100101 Firefox/23.0'
