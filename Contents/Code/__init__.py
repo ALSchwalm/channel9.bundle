63e71d03375fa670c0723f37dc0c5ef067f6e5a1
Adam Schwalm <adamschwalm@gmail.com>
2014-02-08 16:18:32 -0600

    Started adding shows/series

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,24 +1,63 @@
 
 PREFIX = "/video/channel9"
-
 NAME = "Channel9"
+URL = "http://channel9.msdn.com/" 
 
-ART      = 'art-default.jpg'
-ICON     = 'icon-default.png'
+ART = 'art-default.jpg'
+ICON = 'icon-default.png'
 
 ####################################################################################################
 
-# This function is initially called by the PMS framework to initialize the plugin. This includes
-# setting up the Plugin static instance along with the displayed artwork.
 def Start():
 
-    # Setup the default breadcrumb title for the plugin
     ObjectContainer.title1 = NAME
+    HTTP.CacheTime = CACHE_1HOUR
+    HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:23.0) Gecko/20100101 Firefox/23.0'
 
-# This main function will setup the displayed items.
 # Initialize the plugin
 @handler(PREFIX, NAME)
 def MainMenu():
     oc = ObjectContainer()
 
+    oc.add(DirectoryObject(
+        key = Callback(ShowListBrowser, title='Shows', mode=""),
+        title = 'Shows'
+    ))
+
+    oc.add(DirectoryObject(
+        key = Callback(SeriesListBrowser, title='Series', mode=""),
+        title = 'Series'
+    ))
+
     return oc
+
+@route(PREFIX + '/showlist')
+def ShowListBrowser(title, mode):
+
+    oc = ObjectContainer(title2=title)
+    html = HTML.ElementFromURL(URL + "Browse/Shows")
+
+    return oc
+
+@route(PREFIX + '/serieslist')
+def SeriesListBrowser(title, mode):
+
+    oc = ObjectContainer(title2=title)
+    html = HTML.ElementFromURL(URL + "Browse/Series")
+    
+    for show in html.xpath(r'//div[@id="gallery_index_display"]'):
+        title = ""
+        summary = ""
+        url = ""
+
+        oc.add(DirectoryObject(
+            key = Callback(SeriesBrowser, show_url=url, show_name=title),
+            title = title,
+            summary = summary
+        ))
+
+    return oc
+
+@route(PREFIX + '/serieslist/series')
+def SeriesBrowser(series_name, series_url):
+    pass
