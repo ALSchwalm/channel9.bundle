c2e22f61ac6530a96be71b0bafb2fe66415eb65c
Adam Schwalm <adamschwalm@gmail.com>
2014-02-08 14:46:56 -0600

    Channel now appears actuall plex channel

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,8 +1,24 @@
 
-URL = 'http://channel9.msdn.com/'
+PREFIX = "/video/channel9"
 
+NAME = "Channel9"
+
+ART      = 'art-default.jpg'
+ICON     = 'icon-default.png'
+
+####################################################################################################
+
+# This function is initially called by the PMS framework to initialize the plugin. This includes
+# setting up the Plugin static instance along with the displayed artwork.
 def Start():
 
-    ObjectContainer.title1 = 'Channel 9'
-    HTTP.CacheTime = CACHE_1HOUR
-    HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:23.0) Gecko/20100101 Firefox/23.0'
+    # Setup the default breadcrumb title for the plugin
+    ObjectContainer.title1 = NAME
+
+# This main function will setup the displayed items.
+# Initialize the plugin
+@handler(PREFIX, NAME)
+def MainMenu():
+    oc = ObjectContainer()
+
+    return oc
